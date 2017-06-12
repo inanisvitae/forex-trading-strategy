62d8923f083a22389074baf00dc1882dbaa5e32e
kievking <jinlongren100@gmail.com>
2017-06-12 17:17:15 +0800

    Added checking time functionality

--- app/utilities.js
+++ app/utilities.js
@@ -1,6 +1,10 @@
-var urlencode = require('urlencode');
+// var urlencode = require('urlencode');
 
-var x = urlencode('TKNpIUDrIPagJ+Ek/yN1H6GB2Y23ZnWn4EGudvITwQOT6qs2YyhsKqSECH8rFFEirVDC8HY1/oXCFDKXYezsk27oaFX+uCR17rg49cstTioz69+Gg/s4pXjvLvizw4XwNCsqz42wdkt9tg=');
+// var x = urlencode('TKNpIUDrIPagJ+Ek/yN1H6GB2Y23ZnWn4EGudvITwQOT6qs2YyhsKqSECH8rFFEirVDC8HY1/oXCFDKXYezsk27oaFX+uCR17rg49cstTioz69+Gg/s4pXjvLvizw4XwNCsqz42wdkt9tg=');
 
 
-console.log(x);
+// console.log(x);
+
+var moment = require('moment');
+moment().format();
+console.log(12 % 4)
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-
+var moment = require('moment');
 var Strategy = require('./app/strategy/strategy.js');
 
 var strategy = new Strategy();
 
 
 var interval = setInterval(function() {
-	strategy.rsi_macd_strategy();
-}, 14400000);
+	var modulus = moment().hour() % 4
+	if(moment().minute() == 0 && modulus == 0) {
+		strategy.rsi_macd_strategy();
+	}
+}, 25000);
+
